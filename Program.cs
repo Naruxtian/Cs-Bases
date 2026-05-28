f33811ff3b0f262f0353df90496dc3e8309b81bb
naruxtian <naruxdev@gmail.com>
2026-05-27 22:58:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Hello, World!");
+
+        //Declaración explicita
+        int quantity = 5;
+        string message = "Hello, World!";
+        decimal price = 19.99m;
+        WriteLine($"Quantity: {quantity}, Message: {message}, Price: {price:C}");
+
+        //Declaración implícita
+        var grettings = "Hola";
+        var porcentage = 20.00m;
+        WriteLine($"Grettings: {grettings}, Porcentage: {porcentage}%");
+
+    }
+}
